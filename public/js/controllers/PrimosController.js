b28c97f9061d229e120eaafcb2d4efffa1e59b1c
pehls <gabrielpehls@hotmail.com>
2017-09-11 22:46:38 -0300

    erro na escolha de co-primo&MDC

--- public/js/controllers/PrimosController.js
+++ public/js/controllers/PrimosController.js
@@ -21,7 +21,7 @@ angular.module('auditoriaeseguranca').controller('PrimosController',
       );
     }
     buscaPrimos();
-    var Q,P;
+    var Q,P,mensagemC;
       $scope.$watch('selectedP', function(value) {
         console.log($scope.primos[value-1].numero);
         Q=$scope.primos[value-1].numero;
@@ -33,8 +33,11 @@ angular.module('auditoriaeseguranca').controller('PrimosController',
       $scope.decifrar = function () {
         $scope.n = P*Q;
         $scope.z = (P-1)*(Q-1);
-        $scope.e = coPrimo($scope.z);
+        $scope.e = 35;//coPrimo($scope.z);
+        console.log("\n x=" + coPrimo($scope.z));
         $scope.d = d();
+        $scope.en= "("+$scope.e+","+$scope.n+")";
+        $scope.dn= "("+$scope.d+","+$scope.n+")";
         // var BI2 = int2bigInt(2, 1, 1);
         // var BI3 = GCD(BI1, BI2);
         // console.log(BI3);
@@ -46,26 +49,39 @@ angular.module('auditoriaeseguranca').controller('PrimosController',
           }
         }
       }
+      /**
+      *método está errado
+      */
       function coPrimo(x) {
           for (i=0; i<$scope.primos.length; ++i) {
-            if ($scope.primos[i].numero>x+1 ) {
+            console.log("MDC = "+mdc($scope.primos[i].numero,x) + "\n + primo = " + $scope.primos[i].numero + "\n x= " + x);
+            // if ($scope.primos[i].numero>x+1 ) {
               if (mdc($scope.primos[i].numero,x) == 1) {
                 return $scope.primos[i].numero;
               }
-            }
+            // }
           }
       };
       function mdc (x,y) {
-        var resp = 0;
+        var resto = 1;
         do {
           resto = mod(x,y);
-          x=y;
-          y=resto;
-        }while(resto!= 0);
-        return x;
+          x = y;
+          y = resto;
+        } while (resto!=0);
+      return x;
       };
       function mod(x,y) {
         return x%y;
       };
+      $scope.$watch('mensagemCifrada', function(value) {
+      mensagemC = value;
+       $scope.e = 35;
+      // $scope.d  = 13;
+      var mensagemCpwup = mod(Math.pow(mensagemC, $scope.d),$scope.n);
+      console.log("pwup = " + mensagemCpwup);
+      // console.log(bigInt(mensagemC).modPow($scope.d,$scope.n));
+      $scope.mensagemDecifrada = mensagemCpwup;
+      });
 
 });
